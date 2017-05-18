bf409f8adf17ab979374c0015894098a3a4c19c4
artemyo <artemyobort@gmail.com>
2017-05-19 01:07:03 +0300

    test fix categories veiw

--- js/addCategories.js
+++ js/addCategories.js
@@ -40,26 +40,5 @@ AddCategories.prototype.getCategories = function (){
 
 var addCategories = new AddCategories();
 
-	// var getDb = JSON.parse(localStorage.getItem('db'));
-	// var incr = 1;
-	// var id ="id";
-	// var fullId = id + incr;
-	// getDb.categories.forEach(function(category){
-
-	// 	console.log(category.title);
-	// 	var addCategoryLi = markup.create({
-	// 		tag: 'li',
-	// 		parent: 'Categories',
-	// 		id: fullId
-	// 	});
-
-	// 	var adddCategoryA = murkup.create({
-	// 		tag: 'a',
-	// 		content: category.title,
-	// 		parent: fullId,
-	// 		attrs: 'href="#"'
-	// 	});
-
-	// 	incr+=incr;
-	// });
+addCategories.getCategories();
 
