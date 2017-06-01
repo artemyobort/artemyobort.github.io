ea1050f158725ec9fd82734a5ebc9dc8699f6df7
artemyo <artemyobort@gmail.com>
2017-06-02 00:47:00 +0300

    try to add event on categories

--- js/addCategories.js
+++ js/addCategories.js
@@ -11,14 +11,15 @@ AddCategories.prototype.getCategories = function (){
 	var getDb = JSON.parse(data.read('db'));
 
 	var addCategories = document.getElementById('categories');
-	  addCategories.innerHTML = '';
+	addCategories.innerHTML = '';
 
 	var addUl = markup.create({
 	    tag: 'ul',
 	    parent: addCategories
-	  });
+	});
 
-	  getDb.categories.forEach(function(category) {
+	getDb.categories.forEach(function(category) {
+
 	    
 	    var addLi = markup.create({
 	      tag: 'li',
@@ -30,13 +31,83 @@ AddCategories.prototype.getCategories = function (){
 	    	parent: addLi,
 	    	attrs: [
 	    		{type: "button"},
-	    		{onclick: "selectCategory(category)"},
 	    		{value: category.title}
-	    		
 	    	]
 	    });
+		
+		var arrFiltered = [];
+		addInput.onclick = function(event) {
+			arrFiltered = selectCategory(category);
+			// console.log(arrFiltered);
+			// alert(arrFiltered[0].id);
+			var getDb = JSON.parse(data.read('db'));
+
+			for (var i = 0; i < arrFiltered.length; i++){
+				for (var j = 0; j < getDb.products.length; j++) {
+
+					if (getDb.products.category === arrFiltered[0].id){
+						var addGoods = document.getElementById('goods');
+				  		addUdGoods.innerHTML = '';
+
+						var addUl = markup.create({
+							tag: 'ul',
+							parent: addGoods,
+							className: 'products clearfix'
+						});
+
+						getDb.products.forEach(function(product) {
+							var addLi = markup.create({
+								tag: 'li',
+								parent: addUl,
+								className: 'product-wrapper'
+							});
+
+							var addHname = markup.create({
+								tag: 'h4',
+								content: product.title,
+								parent: addLi
+
+							});
+
+							var addA = markup.create({
+								tag: 'a',
+								parent: addLi,
+								className: 'product'
+							});
+
+							var addDiv = markup.create({
+								tag: 'div',
+								parent: addA,
+								className: 'product-photo'
+							});
+
+							var addImg = markup.create({
+								tag: 'img',
+								parent: addDiv,
+								attrs: [
+									{src: "https://pp.userapi.com/c629327/v629327473/db66/r051joYFRX0.jpg"}
+								]
+							});
+
+							var addP = markup.create({
+								tag: 'p',
+								content: product.description,
+								parent: addA
+							});
+
+							var addHprice = markup.create({
+								tag: 'h4',
+								content: product.price,
+								parent: addLi
+							});
+				  		});
+					};  
+				};
+			};
+		};
 	});
 };
+// console.log(arrFiltered);
 
 var addCategories = new AddCategories();
 
--- js/eventCategories.js
+++ js/eventCategories.js
@@ -1,17 +1,15 @@
-function selectCategory (category){
+function selectCategory(category){
 	var db = data.read('db');
   	if (!db) {
 	    db = [];
 	    data.update('db', JSON.stringify(db));
   	}
 
+  	var selectedCategory = category;
   	var getDb = JSON.parse(data.read('db'));
 	var arrNew = getDb.categories.filter(function(category){
-
-		var selectedCategory = category;
-		// console.log(selectedCategory);
-
-		return category.id === selectedCategory.id
+		return selectedCategory.id === category.id
 	});
+		return arrNew;
 };
 
